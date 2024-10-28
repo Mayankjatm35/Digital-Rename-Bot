4e1ca5c33907efa800f90218abae98ba17ba3ca5
Mayankjatm35 <jatmayankjatm35@gmail.com>
2024-10-28 20:35:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class Config(object):
     # digital_botz client config
     API_ID = os.environ.get("API_ID", "23159366")
     API_HASH = os.environ.get("API_HASH", "4623dd30dd1303bddb729eb0862262d9")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8190138294:AAEKb4Ezrin5Q3Hcc5lNdUX6xnytxTYvD6g") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
     # premium account string session required 😢 
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
